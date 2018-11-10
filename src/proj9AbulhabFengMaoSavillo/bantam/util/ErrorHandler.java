ba04ff3d3c1c08f56cc57cb0081a52291cacbab3
zabulhab <zabulhab@colby.edu>
2018-11-09 20:18:30 -0500

    I was forced to commit my javatab thing before pulling

--- src/proj9AbulhabFengMaoSavillo/bantam/util/ErrorHandler.java
+++ src/proj9AbulhabFengMaoSavillo/bantam/util/ErrorHandler.java
@@ -1,26 +1,26 @@
 /* Bantam Java Compiler and Language Toolset.
 
-   Copyright (C) 2009 by Marc Corliss (corliss@hws.edu) and 
+   Copyright (C) 2009 by Marc Corliss (corliss@hws.edu) and
                          David Furcy (furcyd@uwosh.edu) and
                          E Christopher Lewis (lewis@vmware.com).
    ALL RIGHTS RESERVED.
 
-   The Bantam Java toolset is distributed under the following 
+   The Bantam Java toolset is distributed under the following
    conditions:
 
-     You may make copies of the toolset for your own use and 
+     You may make copies of the toolset for your own use and
      modify those copies.
 
-     All copies of the toolset must retain the author names and 
+     All copies of the toolset must retain the author names and
      copyright notice.
 
-     You may not sell the toolset or distribute it in 
-     conjunction with a commerical product or service without 
+     You may not sell the toolset or distribute it in
+     conjunction with a commerical product or service without
      the expressed written consent of the authors.
 
-   THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS 
-   OR IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE 
-   IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A 
+   THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS
+   OR IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE
+   IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
    PARTICULAR PURPOSE.
 
    Modified by Dale Skrien, Fall 2018
